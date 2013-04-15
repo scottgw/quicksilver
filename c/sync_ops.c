d479a28122542cb085006f10ed198baf05a9b97f
Scott West <scott.gregory.west@gmail.com>
2013-04-15 20:50:23 +0200

    Adding sync_data constructor and enqueue/dequeue.
    The enqueue/dequeue are for the runnable tasks list, which is currently
    held as a global variable. More importantly it is currently a non-concurrent
    data structure.

--- c/sync_ops.c
+++ c/sync_ops.c
@@ -12,8 +12,50 @@ struct sleeper
 
 typedef struct sleeper* sleeper_t;
 
+static
 void
-add_sleeper(sync_data_t sync_data, task_t task, struct timespec duration)
+sleep_free(void *item, void *user_data)
+{
+  free(item);
+}
+
+sync_data_t
+sync_data_new(uint64_t max_tasks)
+{
+  sync_data_t result = (sync_data_t)malloc(sizeof(struct sync_data));
+
+  result->max_tasks = max_tasks;
+
+  assert(lfds611_queue_new(&result->runnable_queue, max_tasks) == 1);
+  assert(lfds611_slist_new(&result->sleep_list, sleep_free, NULL) == 1);
+
+  return result;
+}
+
+void
+sync_data_enqueue_runnable(sync_data_t sync_data, task_t task)
+{
+  assert(task != NULL);
+  assert(task->state != TASK_RUNNING);
+  assert(lfds611_queue_enqueue(sync_data->runnable_queue, task) == 1);
+}
+
+task_t
+sync_data_dequeue_runnable(sync_data_t sync_data)
+{
+  task_t task = NULL;
+
+  if (lfds611_queue_dequeue(sync_data->runnable_queue, (void**)&task) == 1)
+    {
+      assert(task->state == TASK_RUNNABLE);
+    }
+
+  return task;  
+}
+
+
+void
+sync_data_add_sleeper(sync_data_t sync_data, task_t task, struct timespec duration)
 {
   conc_list_t sleepers = sync_data->sleep_list;
   sleeper_t sleeper = (sleeper_t) malloc(sizeof(struct sleeper));
@@ -30,9 +72,9 @@ add_sleeper(sync_data_t sync_data, task_t task, struct timespec duration)
 }
 
 void
-get_sleepers(sync_data_t sync_data,
-             task_t **tasks,
-             uint64_t *num_awoken)
+sync_data_get_sleepers(sync_data_t sync_data,
+                       task_t **tasks,
+                       uint64_t *num_awoken)
 {
   conc_list_t sleepers = sync_data->sleep_list;
 
--- c/sync_ops.h
+++ c/sync_ops.h
@@ -6,21 +6,37 @@
 
 #include "task.h"
 
+typedef struct lfds611_queue_state* conc_queue_t;
+
 typedef struct lfds611_slist_state* conc_list_t;
 typedef struct lfds611_slist_element* conc_list_elem_t;
 
 struct sync_data 
 {
+  uint64_t max_tasks;
   conc_list_t sleep_list;
+  conc_queue_t runnable_queue;
 };
 
 typedef struct sync_data* sync_data_t;
 
-void
-add_sleeper(sync_data_t sync_data, task_t task, struct timespec duration);
+
+sync_data_t
+sync_data_new(uint64_t max_tasks);
 
 void
-get_sleepers(sync_data_t sync_data,
+sync_data_enqueue_runnable(sync_data_t sync_data, task_t task);
+
+task_t
+sync_data_dequeue_runnable(sync_data_t sync_data);
+
+void
+sync_data_add_sleeper(sync_data_t sync_data,
+                      task_t task,
+                      struct timespec duration);
+
+void
+sync_data_get_sleepers(sync_data_t sync_data,
              task_t **tasks,
              uint64_t *num_awoken);
 
