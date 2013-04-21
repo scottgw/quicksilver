569d5959234b27ac13d9f43c242972c99948ba6c
Scott West <scott.gregory.west@gmail.com>
2013-04-21 15:13:27 +0200

    Updating basic test.

--- c/tests/basic/basic.c
+++ c/tests/basic/basic.c
@@ -19,29 +19,10 @@
 #define MAX_TASKS 16
 #define N 40
 
-__attribute__ ((noinline))
-static
-void
-false_yield(processor_t proc, int i)
-{
-  return;
-}
-
-int
-fibY(processor_t proc, int i)
-{
-  maybe_yield(proc, i);
-  if (i < 2)
-    return 1;
-  else
-    return fibY(proc, i-1) + fibY(proc, i-2);
-}
-
-
 int
 fib(processor_t proc, int i)
 {
-  false_yield(proc, i);
+  maybe_yield(proc, i);
   if (i < 2)
     return 1;
   else
@@ -49,25 +30,11 @@ fib(processor_t proc, int i)
 }
 
 void
-task3(void* data)
-{
-  processor_t proc = (processor_t)data;
-  struct timespec ts;
-  ts.tv_sec = 1;
-  ts.tv_nsec = 0;
-  printf("Pre sleep\n");
-  proc_sleep(proc, ts);
-  printf("post sleep 1\n");
-  proc_sleep(proc, ts);
-  printf("post sleep 2\n");
-}
-
-void
 task2(void* data)
 {
   int x;
   printf("Thread2 on the go\n");
-  x = fibY((processor_t) data, N);
+  x = fib((processor_t) data, N);
   printf("Result2: %d\n", x);
   printf("Thread2 finished\n");
 }
@@ -77,7 +44,7 @@ task1(void* data)
 {
   int x;
   printf("Thread1 on the go\n");  
-  x = fibY((processor_t) data, N);
+  x = fib((processor_t) data, N);
   printf("Result1: %d\n", x);
   printf("Thread1 finished\n");
 }
@@ -86,25 +53,14 @@ int
 main(int argc, char **argv)
 {
   sync_data_t sync_data = sync_data_new(MAX_TASKS);
-  processor_t proc1;
-  processor_t proc2;
-  processor_t proc3;
-
-
-  /* printf("%d\n", fib(NULL, N)); */
-  /* printf("%d\n", fib(NULL, N)); */
-
-  proc1 = make_processor();
-  proc2 = make_processor();
-  proc3 = make_processor();
+  processor_t proc1 = make_processor();
+  processor_t proc2 = make_processor();
 
   reset_stack_to (task1, proc1);
   reset_stack_to (task2, proc2);
-  reset_stack_to (task3, proc3);
 
   sync_data_enqueue_runnable(sync_data, proc1);
   sync_data_enqueue_runnable(sync_data, proc2);
-  sync_data_enqueue_runnable(sync_data, proc3);
 
   create_executors(sync_data, 2);
 
