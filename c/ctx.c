0a53ca093a6e4fb5eb5945156fe370abac3fb1ba
Scott West <scott.gregory.west@gmail.com>
2013-04-24 10:28:08 +0200

    Using setjmp/longjmp instead of ucontext.
    Hopefully this will lead to a path where syscalls aren't needed to
    switch between tasks.

--- c/ctx.c
+++ c/ctx.c
@@ -1,12 +1,15 @@
 #include <assert.h>
 #include <ucontext.h>
+#include <setjmp.h>
 #include <stdlib.h>
+
 
 #include "ctx.h"
 
 struct ctx
 {
-  ucontext_t ctx;
+  jmp_buf jbuf;
+  ucontext_t uctx;
 };
 
 
@@ -14,7 +17,7 @@ static
 uint32_t
 ctx_get(ctx_t ctx)
 {
-  return getcontext(&ctx->ctx);
+  return getcontext(&ctx->uctx);
 }
 
 
@@ -26,48 +29,77 @@ ctx_new()
   return ctx;
 }
 
+typedef struct
+{
+  ctx_t ctx;
+  ucontext_t next_uctx;
+  void (*func)(void*);
+  void *ptr;
+} ctx_wrapper_data;
+
+static
+void
+ctx_wrapper_f(ctx_wrapper_data* data)
+{
+  if (setjmp(data->ctx->jbuf) == 0)
+    {
+      // First time through we want to jump back to
+      // the setup routine that just made us.
+      ucontext_t wrapper_uctx;
+      swapcontext(&wrapper_uctx, &data->next_uctx);
+    }
+  data->func(data->ptr);
+  free(data);
+}
+
 void
 ctx_make(ctx_t ctx, void (*func)(void*), void* ptr)
 {
-  makecontext(&ctx->ctx, (void (*)())func, 1, ptr);
+  ctx_wrapper_data* data = (ctx_wrapper_data*)malloc(sizeof(ctx_wrapper_data));
+  data->ctx = ctx;
+  data->func = func;
+  data->ptr = ptr;
+  makecontext(&ctx->uctx, (void (*)())ctx_wrapper_f, 1, data);
+  swapcontext(&data->next_uctx, &ctx->uctx);
 }
 
 void
 ctx_set_next(ctx_t ctx, ctx_t next_ctx)
 {
-  ctx->ctx.uc_link = &next_ctx->ctx;
+  ctx->uctx.uc_link = &next_ctx->uctx;
 }
 
 bool
 ctx_save(ctx_t ctx)
 {
-  volatile int flag = true;
-  assert (ctx_get(ctx) == 0);
-  if (flag)
+  /* volatile int flag = true; */
+  /* assert (ctx_get(ctx) == 0); */
+  if (setjmp(ctx->jbuf) == 0)
     {
-      flag = false;
+      return true;
     }
   else
     {
-      flag = true;
+      return false;
     }
-  return !flag;
 }
 
 uint32_t
 ctx_set(ctx_t ctx)
 {
-  return setcontext(&ctx->ctx);
+  longjmp(ctx->jbuf, 1);
+  assert (false && "ctx_set: should not reach this point after longjmp");
+  return 0;
 }
 
 void
 ctx_set_stack_ptr(ctx_t ctx, void* ptr)
 {
-  ctx->ctx.uc_stack.ss_sp = ptr;
+  ctx->uctx.uc_stack.ss_sp = ptr;
 }
 
 void
 ctx_set_stack_size(ctx_t ctx, size_t sz)
 {
-  ctx->ctx.uc_stack.ss_size = sz;
+  ctx->uctx.uc_stack.ss_size = sz;
 }
