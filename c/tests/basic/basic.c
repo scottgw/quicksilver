7fc1c25585bb536fc685b333fa7aa9d128209525
Scott West <scott.gregory.west@gmail.com>
2013-04-27 11:18:58 +0200

    The basic example uses compiler-centric operations.
    Still not working, as there are still a few shutdown issues to handle,
    so it completes the work, but then it just idles.

--- c/tests/basic/basic.c
+++ c/tests/basic/basic.c
@@ -4,11 +4,15 @@
 #include <unistd.h>
 #include <stdint.h>
 
+#include "bounded_queue.h"
 #include "executor.h"
+#include "maybe.h"
 #include "notifier.h"
 #include "processor.h"
+#include "task.h"
 #include "list.h"
 #include "task_mutex.h"
+#include "closure.h"
 
 #include "sync_ops.h"
 
@@ -29,6 +33,7 @@ fib(processor_t proc, int i)
     return fib(proc, i-1) + fib(proc, i-2);
 }
 
+
 void
 task2(void* data)
 {
@@ -46,45 +51,60 @@ task1(void* data)
 {
   int x;
   processor_t proc = (processor_t)data;
-  printf("Thread1 on the go\n");  
+  printf("Thread1 on the go\n");
   x = fib(proc, N);
   printf("Result1: %d\n", x);
   printf("Thread1 finished\n");
   sync_data_deregister_proc(proc->task->sync_data);
 }
 
+
+void
+proc_main(void* data)
+{
+  processor_t proc = (processor_t) data;
+
+  processor_t proc1 = make_processor(proc->task->sync_data);
+  processor_t proc2 = make_processor(proc->task->sync_data);
+
+  bounded_queue_t q1 = proc_make_private_queue(proc1);
+  bounded_queue_t q2 = proc_make_private_queue(proc2);
+
+  closure_t clos1 = closure_new(task1, proc1);
+  closure_t clos2 = closure_new(task2, proc2);
+
+  maybe_t m1 = (maybe_t) malloc(sizeof(struct maybe));
+  maybe_t m2 = (maybe_t) malloc(sizeof(struct maybe));
+
+  m1->load = clos1;
+  m2->load = clos2;
+
+  bqueue_enqueue(q1, m1);
+  bqueue_enqueue(q2, m2);
+
+}
+
 int
 main(int argc, char **argv)
 {
   sync_data_t sync_data = sync_data_new(MAX_TASKS);
-  processor_t proc1 = make_processor(sync_data);
-  processor_t proc2 = make_processor(sync_data);
-
-  reset_stack_to (task1, proc1);
-  reset_stack_to (task2, proc2);
-
-  sync_data_register_proc(sync_data);
-  sync_data_register_proc(sync_data);
-
-  sync_data_enqueue_runnable(sync_data, proc1);
-  sync_data_enqueue_runnable(sync_data, proc2);
+  processor_t proc = make_processor(sync_data);
 
   create_executors(sync_data, 2);
 
+  bounded_queue_t q = proc_make_private_queue(proc);
+  closure_t clos = closure_new(proc_main, proc);
+  maybe_t m = (maybe_t) malloc(sizeof(struct maybe));
+  m->load = clos;
+
+  bqueue_enqueue(q, m);
+
   {
     notifier_t notifier = notifier_spawn(sync_data);
-    pthread_join(notifier->thread, NULL);
+    notifier_join(notifier);
   }
 
   join_executors();
-
-  printf("post join\n");
-
-  /* list_free(work); */
-  free_processor(proc1);
-  free_processor(proc2);
-
-  printf("end of test\n");
 
   return 0;
 }
