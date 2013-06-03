9840113d3b9f7f33d42c0b19d4b536ffa2b332dc
Scott West <scott.gregory.west@gmail.com>
2013-06-03 10:17:24 +0200

    Removing unused modules from LLVM.

--- compiler/exec/Language/QuickSilver/Generate/LLVM/Values.hs
+++
@@ -1,8 +0,0 @@
-module Language.QuickSilver.Generate.LLVM.Values where
-
-import Data.Char
-
-import Control.Applicative
-
-import Language.QuickSilver.Generate.LLVM.Simple
-import Language.QuickSilver.Generate.LLVM.Types
