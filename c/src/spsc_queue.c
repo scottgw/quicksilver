6b327461a1afce38e8fe8ffc6ad9567f56508448
Scott West <scott.gregory.west@gmail.com>
2013-06-10 07:11:54 +0200

    Adding #ifdef implementation change to spsc queue.

--- c/src/spsc_queue.c
+++ c/src/spsc_queue.c
@@ -10,6 +10,8 @@
 #include "libqs/task_mutex.h"
 #include "libqs/task_condition.h"
 
+// #define SPSC_CUSTOM
+
 struct spsc_queue
 {
   uint32_t max;
@@ -17,7 +19,12 @@ struct spsc_queue
 
   volatile processor_t waiter;
 
+#ifdef SPSC_CUSTOM
+  queue_impl_t impl;
+#else
   bounded_queue_t impl;
+#endif
+
 };
 
 spsc_queue_t
@@ -28,7 +35,11 @@ spsc_new(uint32_t size)
   q->count  = 0;
   q->max    = size;
   q->waiter = NULL;
+#ifdef SPSC_CUSTOM
+  q->impl   = queue_impl_new (size);
+#else
   q->impl   = bqueue_new(size);
+#endif
 
   return q;
 }
@@ -36,89 +47,91 @@ spsc_new(uint32_t size)
 void
 spsc_free(spsc_queue_t q)
 {
+#ifdef SPSC_CUSTOM
+  queue_impl_free(q->impl);
+#else
   bqueue_free(q->impl);
+#endif
   free(q);
 }
 
-
+#ifdef SPSC_CUSTOM
 void
 spsc_enqueue_wait(spsc_queue_t q, void *data, processor_t proc)
 {
-  bqueue_enqueue_wait(q->impl, data, proc);
+  int n = __sync_fetch_and_add(&q->count, 1);
+  if (n == -1)
+    {
+      bool success = queue_impl_enqueue(q->impl, data);
+      assert(success);
+
+      while (q->waiter == NULL);
+
+      processor_t waiter = q->waiter;
+      q->waiter = NULL;
+      proc_wake(waiter);
+    }
+  else if (n == q->max)
+    {
+      q->waiter = proc;
+      task_set_state(proc->task, TASK_TRANSITION_TO_WAITING);
+      proc_yield_to_executor(proc);
+
+      bool success = queue_impl_enqueue(q->impl, data);
+      assert(success);
+    }
+  else
+    {
+      while(!queue_impl_enqueue(q->impl, data));
+      /* bool success = queue_impl_enqueue(q->impl, data); */
+      /* assert(success); */
+    }
 }
 
 
 void
 spsc_dequeue_wait(spsc_queue_t q, void **data, processor_t proc)
 {
-  bqueue_dequeue_wait(q->impl, data, proc);
+  int n = __sync_fetch_and_sub(&q->count, 1);
+  if (n == q->max + 1)
+    {
+      bool success = queue_impl_dequeue(q->impl, data);
+      assert(success);
+
+      while (q->waiter == NULL);
+
+      // At this point no other processors can be in the queue.
+      processor_t waiter = q->waiter;
+      q->waiter = NULL;
+      proc_wake(waiter);
+    }
+  else if (n == 0)
+    {
+      q->waiter = proc;
+      task_set_state(proc->task, TASK_TRANSITION_TO_WAITING);
+      proc_yield_to_executor(proc);
+
+      bool success = queue_impl_dequeue(q->impl, data);
+      assert(success);
+    }
+  else
+    {
+      while(!queue_impl_dequeue(q->impl, data));
+      /* bool success = queue_impl_dequeue(q->impl, data); */
+      /* assert(success); */
+    }
+  
+}
+#else
+void
+spsc_enqueue_wait(spsc_queue_t q, void *data, processor_t proc)
+{
+  bqueue_enqueue_wait(q->impl, data, proc);
 }
 
-
-/* void */
-/* spsc_enqueue_wait(spsc_queue_t q, void *data, processor_t proc) */
-/* { */
-/*   int n = __sync_fetch_and_add(&q->count, 1); */
-/*   if (n == -1) */
-/*     { */
-/*       bool success = queue_impl_enqueue(q->impl, data); */
-/*       assert(success); */
-
-/*       while (q->waiter == NULL); */
-
-/*       processor_t waiter = q->waiter; */
-/*       q->waiter = NULL; */
-/*       proc_wake(waiter); */
-/*     } */
-/*   else if (n == q->max) */
-/*     { */
-/*       q->waiter = proc; */
-/*       task_set_state(proc->task, TASK_TRANSITION_TO_WAITING); */
-/*       proc_yield_to_executor(proc); */
-
-/*       bool success = queue_impl_enqueue(q->impl, data); */
-/*       assert(success); */
-/*     } */
-/*   else */
-/*     { */
-/*       while(!queue_impl_enqueue(q->impl, data)); */
-/*       /\* bool success = queue_impl_enqueue(q->impl, data); *\/ */
-/*       /\* assert(success); *\/ */
-/*     } */
-/* } */
-
-
-/* void */
-/* spsc_dequeue_wait(spsc_queue_t q, void **data, processor_t proc) */
-/* { */
-/*   int n = __sync_fetch_and_sub(&q->count, 1); */
-/*   if (n == q->max + 1) */
-/*     { */
-/*       bool success = queue_impl_dequeue(q->impl, data); */
-/*       assert(success); */
-
-/*       while (q->waiter == NULL); */
-
-/*       // At this point no other processors can be in the queue. */
-/*       processor_t waiter = q->waiter; */
-/*       q->waiter = NULL; */
-/*       proc_wake(waiter); */
-/*     } */
-/*   else if (n == 0) */
-/*     { */
-/*       q->waiter = proc; */
-/*       task_set_state(proc->task, TASK_TRANSITION_TO_WAITING); */
-/*       proc_yield_to_executor(proc); */
-
-/*       bool success = queue_impl_dequeue(q->impl, data); */
-/*       assert(success); */
-/*     } */
-/*   else */
-/*     { */
-/*       while(!queue_impl_dequeue(q->impl, data)); */
-/*       /\* bool success = queue_impl_dequeue(q->impl, data); *\/ */
-/*       /\* assert(success); *\/ */
-/*     } */
-  
-/* } */
-
+void
+spsc_dequeue_wait(spsc_queue_t q, void **data, processor_t proc)
+{
+  bqueue_dequeue_wait(q->impl, data, proc);
+}
+#endif
