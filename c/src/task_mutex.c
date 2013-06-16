1b21da4b9d4ce2ce8a6f3ddfcb2a77875285a5c2
Scott West <scott.gregory.west@gmail.com>
2013-06-16 20:53:43 +0200

    Use the old bounded queue.
    The queue didn't appear to be a bottle-neck,
    but it's nice to have the option later if it does become one.

--- c/src/task_mutex.c
+++ c/src/task_mutex.c
@@ -3,7 +3,8 @@
 
 #include "libqs/debug_log.h"
 #include "libqs/processor.h"
-#include "libqs/mpsc_impl.h"
+/* #include "libqs/mpsc_impl.h" */
+#include "libqs/queue_impl.h"
 #include "libqs/sync_ops.h"
 #include "libqs/task.h"
 #include "libqs/task_mutex.h"
@@ -16,7 +17,8 @@ struct task_mutex
   volatile uint32_t count;
   processor_t owner;
   /* struct mpscq_node_t stub; */
-  mpscq_t wait_queue;
+  /* mpscq_t wait_queue; */
+  queue_impl_t wait_queue;
 };
 
 task_mutex_t
@@ -25,14 +27,15 @@ task_mutex_new()
   task_mutex_t mutex = (task_mutex_t)malloc(sizeof(struct task_mutex));
   mutex->count = 0;
   mutex->owner = NULL;
-  mpscq_create(&mutex->wait_queue, NULL); // mutex->stub);
-
+  /* mpscq_create(&mutex->wait_queue, NULL); // mutex->stub); */
+  mutex->wait_queue = queue_impl_new (INIT_WAIT_QUEUE_SIZE);
   return mutex;
 }
 
 void
 task_mutex_free(task_mutex_t mutex)
 {
+  queue_impl_free (mutex->wait_queue);
   free (mutex);
 }
 
@@ -41,52 +44,6 @@ task_mutex_owner(task_mutex_t mutex)
 {
   return mutex->owner;
 }
-
-/* void */
-/* task_mutex_lock(task_mutex_t mutex, processor_t proc) */
-/* { */
-/*  if (__sync_fetch_and_add(&mutex->count, 1) > 0) */
-/*     { */
-/*       // if the owner is already set then we add to the wait list */
-/*       // and yield to the executor. */
-/*       proc->task->state = TASK_TRANSITION_TO_WAITING; */
-/*       assert(queue_impl_enqueue(mutex->wait_queue, proc)); */
-/*       yield_to_executor(proc); */
-/*     } */
-/*  else */
-/*    { */
-/*      // if not, set the owner. */
-/*      __sync_synchronize(); */
-/*      mutex->owner = proc; */
-/*    } */
-/* } */
-
-/* void */
-/* task_mutex_unlock(task_mutex_t mutex, processor_t proc) */
-/* { */
-/*   processor_t other_proc = NULL; */
-/*   assert(mutex->owner == proc); */
-  
-/*   if (__sync_fetch_and_sub(&mutex->count, 1) > 1) */
-/*     { */
-/*       // if there's someone in the loop, spin to dequeue them from the wait-queue. */
-/*       // spinning is OK here because we only have to wait between when they */
-/*       // increased the counter and when the enqueue themselves which */
-/*       // is a finite amount of time. */
-/*       while(!queue_impl_dequeue(mutex->wait_queue, (void**)&other_proc)); */
-/*       __sync_synchronize(); */
-/*       while(other_proc->task->state != TASK_WAITING); */
-/*       mutex->owner = other_proc; */
-/*       task_set_state(other_proc->task, TASK_RUNNABLE); */
-/*       sync_data_enqueue_runnable(proc->task->sync_data, other_proc); */
-/*     } */
-/*   else */
-/*     { */
-/*       // If there were zero people here, then just set the owner to NULL */
-/*       __sync_synchronize(); */
-/*       mutex->owner = NULL; */
-/*     } */
-/* } */
 
 void
 task_mutex_lock(task_mutex_t mutex, processor_t proc)
@@ -106,7 +63,9 @@ task_mutex_lock(task_mutex_t mutex, processor_t proc)
       /* proc->node.state = proc; */
 
       task_set_state(proc->task, TASK_TRANSITION_TO_WAITING);
-      mpscq_push(&mutex->wait_queue, proc);
+
+      queue_impl_enqueue(mutex->wait_queue, proc);
+      /* mpscq_push(&mutex->wait_queue, proc); */
 
       proc_yield_to_executor(proc);
 
@@ -133,12 +92,14 @@ task_mutex_unlock(volatile task_mutex_t mutex, processor_t proc)
       /* mpscq_node_t* node; */
       processor_t other_proc;
 
-      do
-        {
-          other_proc = mpscq_pop(&mutex->wait_queue);
-        } while(other_proc == 0);
+      /* do */
+      /*   { */
+      /*     other_proc = mpscq_pop(&mutex->wait_queue); */
+      /*   } while(other_proc == 0); */
 
-       /* = (processor_t) node->state; */
+      /*  /\* = (processor_t) node->state; *\/ */
+
+      queue_impl_dequeue(mutex->wait_queue, (void**)&other_proc);
 
       while (other_proc->task->state != TASK_WAITING);
 
