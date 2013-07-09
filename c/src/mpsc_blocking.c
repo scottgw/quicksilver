525c24a15eed598440c6cd71d2b64864f57bf1a1
Scott West <scott.gregory.west@gmail.com>
2013-07-09 10:44:19 +0200

    Cleaning up indentation in mpsc_blocking.c.

--- c/src/mpsc_blocking.c
+++ c/src/mpsc_blocking.c
@@ -9,117 +9,54 @@
 #include "libqs/types.h"
 
 struct closq_node
-{ 
-    struct closq_node* volatile  next; 
-    void*                        state; 
-
-}; 
+{
+  struct closq_node* volatile next;
+  void*                       state;
+};
 
 typedef struct closq_node closq_node_t;
 
 struct closq
-{ 
-    closq_node_t* volatile  head; 
-    closq_node_t*           tail; 
-}; 
+{
+  closq_node_t* volatile head;
+  closq_node_t*          tail;
+};
 
 typedef struct closq closq_t;
 
-void closq_create(closq_t* self, closq_node_t* stub) 
-{ 
-    stub->next = 0; 
-    self->head = stub; 
-    self->tail = stub; 
-} 
-
-void closq_push(closq_t* self, closq_node_t* n) 
-{ 
-    n->next = 0; 
-    closq_node_t* prev;
-    __atomic_exchange(&self->head, &n, &prev,  __ATOMIC_SEQ_CST);
-    prev->next = n; // serialization-point wrt consumer, release
-} 
-
-closq_node_t* closq_pop(closq_t* self) 
-{ 
-    closq_node_t* tail = self->tail; 
-    closq_node_t* next = tail->next; // serialization-point wrt producers, acquire
-    if (next) 
-    { 
-        self->tail = next; 
-        tail->state = next->state; 
-        return tail; 
-    } 
-    return 0; 
+void closq_create(closq_t* self, closq_node_t* stub)
+{
+  stub->next = 0;
+  self->head = stub;
+  self->tail = stub;
 }
- 
-/* struct closq */
-/* { */
-/*   volatile priv_queue_t  head; */
-/*   priv_queue_t           tail; */
-/*   struct priv_queue      stub; */
-/* }; */
 
-/* typedef struct closq closq_t; */
+void closq_push(closq_t* self, closq_node_t* n)
+{
+  n->next = 0;
+  closq_node_t* prev;
+  __atomic_exchange(&self->head, &n, &prev,  __ATOMIC_SEQ_CST);
+  prev->next = n; // serialization-point wrt consumer, release
+}
 
-/* static */
-/* void */
-/* closq_create(closq_t* self)  */
-/* {  */
-/*   self->head = &self->stub; */
-/*   self->tail = &self->stub; */
-/*   self->stub.next = 0; */
-/* }  */
-
-/* static */
-/* void */
-/* closq_push(closq_t* self, priv_queue_t n) */
-/* { */
-/*   n->next = 0; */
-/*   priv_queue_t prev; */
-/*   __atomic_exchange(&self->head, &n, &prev,  __ATOMIC_SEQ_CST); */
-/*   //(*) */
-/*   prev->next = n; */
-/* } */
-
-/* static */
-/* priv_queue_t */
-/* closq_pop(closq_t* self) */
-/* { */
-/*     priv_queue_t tail = self->tail; */
-/*     priv_queue_t next = tail->next; */
-/*     if (tail == &self->stub) */
-/*     { */
-/*         if (0 == next) */
-/*             return 0; */
-/*         self->tail = next; */
-/*         tail = next; */
-/*         next = next->next; */
-/*     } */
-/*     if (next) */
-/*     { */
-/*         self->tail = next; */
-/*         return tail; */
-/*     } */
-/*     priv_queue_t head = self->head; */
-/*     if (tail != head) */
-/*         return 0; */
-/*     closq_push(self, &self->stub); */
-/*     next = tail->next; */
-/*     if (next) */
-/*     { */
-/*         self->tail = next; */
-/*         return tail; */
-/*     } */
-/*     return 0; */
-/* } */
-
+closq_node_t* closq_pop(closq_t* self)
+{
+  closq_node_t* tail = self->tail;
+  closq_node_t* next = tail->next; // serialization-point wrt producers, acquire
+  if (next)
+    {
+      self->tail = next;
+      tail->state = next->state;
+      return tail;
+    }
+  return 0;
+}
 
 struct qo_queue
 {
   volatile uint32_t count;
   volatile processor_t waiter;
-  
+
   mpscq_t producers;
 
   uint64_t max;
@@ -196,8 +133,8 @@ qo_q_dequeue_wait(qo_queue_t q, void **data, processor_t proc)
     }
   else if (n == 0)
     {
-      q->waiter = proc;
       task_set_state(proc->task, TASK_TRANSITION_TO_WAITING);
+      q->waiter = proc;
       proc_yield_to_executor(proc);
 
       node = closq_pop(q->impl);
