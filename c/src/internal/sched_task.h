de4b4c2ed0ffdb1a82c8a54d9685a0c4bb97b457
Scott West <scott.gregory.west@gmail.com>
2013-08-23 19:36:15 +0200

    Shifting implementations from processor to stask.
    The implementations were just renamed previously, but left in processor.c.
    Now they have been moved to sched_task.c.
    
    Also some associated dangling declarations in processor.h have been removed.

--- c/src/internal/sched_task.h
+++ c/src/internal/sched_task.h
@@ -19,18 +19,40 @@ stask_new(sync_data_t);
 void
 stask_free(sched_task_t);
 
+/*!
+  Step the state of the task.
+  This is used when the task is in a transitional state
+  and must be moved to its final state.
+
+  \param stask schedulable task to step
+  \param exec executor to push the task into if its runnable
+*/
 void
-stask_step_state(sched_task_t, executor_t);
+stask_step_state(sched_task_t stask, executor_t exec);
 
 void
 stask_step_previous(sched_task_t stask);
 
+
+/*!
+  Yield the task to its executor.
+  
+  \param stask task to yield.
+*/
 void
 stask_yield_to_executor(sched_task_t);
 
 void
 stask_switch(sched_task_t from, sched_task_t to);
 
+/*!
+  Wake the task up on the given executor.
+  The executor should be the one that is currently active,
+  this is meant to be run from a current running schedulable task.
+
+  \param stask the schedulable task to wake
+  \param exec the executor_t that it will be awoken on.
+*/
 void
 stask_wake(sched_task_t stask, executor_t exec);
 
--- c/src/sched_task.c
+++ c/src/sched_task.c
@@ -1,4 +1,7 @@
+#include <assert.h>
 #include <stdlib.h>
+
+#include "libqs/sync_ops.h"
 
 #include "internal/executor.h"
 #include "internal/sched_task.h"
@@ -24,6 +27,107 @@ stask_free(sched_task_t stask)
   free(stask);
 }
 
+
+/*!
+  Look at the previous processor and runs proc_step_state on it
+  if it is not the argument processor.
+
+  \param proc processor whose executor will be checked for the
+  previous processor.
+*/
+void
+stask_step_previous(sched_task_t stask)
+{
+  executor_t exec = stask->executor;
+  exec_step_previous (exec, stask);
+}
+
+void
+stask_step_state(sched_task_t stask, executor_t exec)
+{
+  assert(stask->task->state >= TASK_TRANSITION_TO_WAITING);
+
+  switch (stask->task->state)
+    {
+    case TASK_TRANSITION_TO_RUNNABLE:
+      stask->task->state = TASK_RUNNABLE;
+      exec_push(exec, stask);
+      break;
+    case TASK_TRANSITION_TO_WAITING:
+      /* printf("%p set to waiting\n", stask); */
+      stask->task->state = TASK_WAITING;
+      break;
+    case TASK_TRANSITION_TO_FINISHED:
+      stask->task->state = TASK_FINISHED;
+      assert("How to free the processor? cast up?" && 0);
+      stask_free(stask);
+      break;
+    default:
+      break;
+    }
+}
+
+
+void
+stask_switch(sched_task_t from, sched_task_t to)
+{
+  assert (from != to);
+  executor_t exec = from->executor;
+  exec->current_stask = from;
+
+  if (to == NULL)
+    {
+      task_switch(from->task, exec->stask->task);
+    }
+  else
+    {
+      to->executor = exec;
+
+      /* // If this task is to finish, it should come back to this processor. */
+      /* // This decision may be dubious, perhaps it should switch the the */
+      /* // executor proper? */
+      /* if (proc->task->state == TASK_TRANSITION_TO_RUNNABLE) */
+      /*   { */
+      /*     next_proc->task->next = proc->task; */
+      /*   } */
+      /* else */
+      /*   { */
+      /*     next_proc->task->next = exec->task; */
+      /*   } */
+
+      to->task->next = exec->stask->task;
+
+      /* printf("%p directly yielding to %p\n", from, to); */
+      // Switch to the task we found.
+      task_switch(from->task, to->task);
+    }
+
+  stask_step_previous(from);
+}
+
+void
+stask_yield_to_executor(sched_task_t stask)
+{
+  assert (stask->task->state >= TASK_TRANSITION_TO_WAITING);
+  executor_t exec = stask->executor;
+  sched_task_t next_stask;
+
+  if (sync_data_try_dequeue_runnable (stask->sync_data, exec, &next_stask))
+    {
+      goto yield;
+    }
+
+  if (exec_steal(exec, &next_stask))
+    {
+      goto yield;
+      /* printf("%p stole %p\n", proc, next_proc); */
+    }
+
+  next_stask = exec_get_work(exec, 8);
+
+ yield:
+  stask_switch(stask, next_stask);
+}
 
 void
 stask_wake(sched_task_t stask, executor_t exec)
