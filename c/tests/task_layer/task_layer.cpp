714a9c3ea8e5be647ee253413dd1b6ac6d7ee77a
Scott West <scott@localhost.localdomain>
2013-08-23 16:10:08 +0200

    Adding first set of tests for refactored code.

---
+++ c/tests/task_layer/task_layer.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include <libqs/task.h>
+
+TEST(Task, MakeFree)
+{
+  task_t task = task_make();
+  task_free(task);
+  ASSERT_TRUE(true);
+}
+
+// This upsets gtest a little, and to not clutter the output I've commented it out.
+// TEST(TaskTest, FreeBad)
+// {
+//   ASSERT_DEATH(task_free(NULL), "");
+// }
+
+
+int
+main(int argc, char** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
