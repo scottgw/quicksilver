6512cf27aabb022ef8c74aad06b34f7c0c6af368
Scott West <scott.gregory.west@gmail.com>
2013-08-24 22:55:11 +0200

    Adding multi-task test.

--- c/tests/sched_layer/sched_layer.cpp
+++ c/tests/sched_layer/sched_layer.cpp
@@ -34,18 +34,18 @@ TEST(SchedTask, ExecutorsMakeFree)
   ASSERT_TRUE(true);
 }
 
-int x;
+int schedSingleRan;
 
 void
 schedule_single(void* data)
 {
   sched_task_t stask = (sched_task_t) stask;
-  x = 1;
+  schedSingleRan = true;
 }
 
 TEST(SchedTask, ScheduleSingle)
 {
-  x = 0;
+  schedSingleRan = false;
   sync_data_t sync_data = sync_data_new(1);
   sync_data_create_executors (sync_data, 1);
   sched_task_t stask = stask_new(sync_data);
@@ -53,11 +53,50 @@ TEST(SchedTask, ScheduleSingle)
 
   sync_data_enqueue_runnable(sync_data, stask);
 
-  // Tasks that finish are cleaned up by another task or the executor.
+  // We do not free the stask because it is cleaned up as it finishes
+  // execution.
   sync_data_join_executors(sync_data);
   sync_data_free(sync_data);
-  ASSERT_EQ(x, 1);
+  ASSERT_EQ(schedSingleRan, true);
 }
+
+
+#define SCHEDULE_MULTIPLE_NUM 20
+
+bool sched_multi_array[SCHEDULE_MULTIPLE_NUM];
+
+void
+schedule_multiple(void* data)
+{
+  bool* flag = (bool*) data;
+  *flag = true;
+}
+
+TEST(SchedTask, ScheduleMultiple)
+{
+  schedSingleRan = false;
+  sync_data_t sync_data = sync_data_new(SCHEDULE_MULTIPLE_NUM);
+  sync_data_create_executors (sync_data, 1);
+
+  for (int i = 0; i < SCHEDULE_MULTIPLE_NUM; i++)
+    {
+      sched_task_t stask = stask_new(sync_data);
+      sched_multi_array[i] = false;
+      task_set_func(stask->task, schedule_multiple, &sched_multi_array[i]);
+      
+      sync_data_enqueue_runnable(sync_data, stask);
+    }
+
+
+  sync_data_join_executors(sync_data);
+  sync_data_free(sync_data);
+
+  for (int i = 0; i < SCHEDULE_MULTIPLE_NUM; i++)
+    {
+      ASSERT_EQ(sched_multi_array[i], true);
+    }
+}
+
 
 int
 main(int argc, char** argv)
