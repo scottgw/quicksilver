6937980a2247a765b2bc225e35e685970af388cf
Scott West <scott.gregory.west@gmail.com>
2013-08-31 10:56:45 +0200

    Adding extern "C" for task_condition.h.

--- c/src/internal/task_condition.h
+++ c/src/internal/task_condition.h
@@ -3,6 +3,10 @@
 
 #include "../libqs/types.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 task_condition_t
 task_condition_new();
 
@@ -17,5 +21,9 @@ task_condition_signal_all(task_condition_t cv, sched_task_t curr_stask);
 
 void
 task_condition_wait(task_condition_t cv, task_mutex_t mutex, sched_task_t prop);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // _TASK_CONDITION_H
