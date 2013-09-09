19a51f7cf43d66bd2b91c78b67b1237bb85a3e20
Scott West <scott.gregory.west@gmail.com>
2013-09-09 13:01:10 +0200

    Lifting out cruft from randmat.

--- benchmarks/parallel/haskell/randmat/Main.hs
+++ benchmarks/parallel/haskell/randmat/Main.hs
@@ -1,72 +1,39 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-module Main where
+module Main (main) where
 
-import           Control.Concurrent.Async
 import           Control.DeepSeq
-import           Control.Monad.Identity
-import           Control.Parallel
+import           Control.Parallel.Strategies
 
-import qualified Data.Array.Repa as Repa
-import           Data.Array.Repa (U, D, DIM1, DIM2, (:.)(..), Z(..))
-import qualified Data.Vector as V
-import qualified Data.Vector.Unboxed as Unbox (unsafeFreeze)
-import qualified Data.Vector.Unboxed.Mutable as Unbox
-import           Data.Vector.Strategies
+import qualified Data.Vector.Unboxed as Unbox
 import           Data.Word
 
 import           System.Environment
 
--- randmat :: Int -> Int -> Repa.Array U DIM2 Int
--- randmat s n = runIdentity $
---   do aa <- Repa.computeP (Repa.map createRow a)
---      Repa.computeP (flattened aa)
---   where
---     arr :: Repa.Array U DIM2 Int
---     arr = Repa.fromUnboxed (Z :. n :. 1) (Unbox.generate n (+s))
 
---     step a = Repa.traverse a (\ (Z :. n :. i) -> Z :. n :. i + 1) go
---       where
---         go at (Z :. 
-
---     flattened :: Repa.Array D DIM1 (Unbox.Vector Int) -> Repa.Array D DIM2 Int
---     flattened v = Repa.traverse v (\ (Z :. n) -> Z :. n :. n) lookupInVec
---       where
---         lookupInVec at (Z :. i :. j) = Unbox.unsafeIndex (at (Z :. i)) j
-
---     createRow :: Int -> Unbox.Vector Int
---     createRow i = Unbox.unfoldrN n go (fromIntegral i)
-
---     go :: Word32 -> Maybe (Int, Word32)
---     go !seed = Just (fromIntegral (nextSeed `rem` 100), nextSeed)
---       where !nextSeed = rand seed
-
-
-randmat :: Int -> Int -> IO (Repa.Array U DIM2 Int)
+randmat :: Int -> Int -> Unbox.Vector Int
 randmat s n = 
-  do mv <- Unbox.new (n * n)
-     let w = 32 -- Number of workers
+    Unbox.concat (map createRow [0 .. n - 1] `using` parListChunk 128 rseq)
+  where
+    createRow i = Unbox.unfoldrN n go (fromIntegral i)
 
-         height | w > n = 1
-                | otherwise = n `div` w
+    go :: Word32 -> Maybe (Int, Word32)
+    go !seed = Just (fromIntegral (nextSeed `rem` 100), nextSeed)
+      where !nextSeed = rand seed
 
-         startIndices | w > n     = [0 .. n - 1]
-                      | otherwise = [i*height | i <- [0.. w - 1]]
+    -- createRow i = Unbox.create (fill i)
 
-         fill startIndex = mapM_ go1 [startIndex .. startIndex + height - 1]
-           where
-             go1 !i = go2 (fromIntegral s + fromIntegral i) 0
-               where
-                 go2 :: Word32 -> Int -> IO ()
-                 go2 !seed !j =
-                   do Unbox.unsafeWrite mv (n * i + j) (fromIntegral (seed `rem` 100))
-                      if j >= n
-                        then return ()
-                        else go2 (rand seed) (j+1)
-
-     mapConcurrently fill startIndices
-     v <- Unbox.unsafeFreeze mv
-     return (Repa.fromUnboxed (Z :. n :. n) v)
+    -- fill startIndex = MUnbox.new n >>= \ mv -> go1 startIndex mv >> return mv
+    --     where
+    --       go1 !i mv = go2 (fromIntegral s + fromIntegral i) 0
+    --           where
+    --             go2 !seed !j =
+    --                 do MUnbox.unsafeWrite mv j (fromIntegral (seed `rem` 100))
+    --                    if j >= n
+    --                    then return ()
+    --                    else go2 (rand seed) (j+1)
 
 rand :: Word32 -> Word32
 rand s = lcg_a * s + lcg_c
@@ -76,5 +43,5 @@ rand s = lcg_a * s + lcg_c
 
 main =
   do [seed, n] <- (map read) `fmap` getArgs
-     x <- randmat seed n
-     print (Repa.extent $ Repa.deepSeqArray x x)
+     let x = randmat seed n
+     print (Unbox.length x)
