e26e547138949a5308d82a67b3300a4a9b377369
Scott West <scott.gregory.west@gmail.com>
2013-09-11 09:30:28 +0200

    run_all update to work with erlang, remove go from list.

--- benchmarks/run_all.py
+++ benchmarks/run_all.py
@@ -5,7 +5,7 @@ import time
 import csv
 from subprocess import Popen, call, PIPE
 
-langs=['cxx', 'haskell', 'qs', 'go', 'erlang']
+langs=['cxx', 'haskell', 'qs', 'erlang']
 
 tasks={'concurrent': ['condition', 'mutex', 'prodcons', 'threadring', 'chameneos'],
        'parallel': ['randmat', 'thresh', 'winnow', 'outer', 'product']}
@@ -13,7 +13,7 @@ tasks={'concurrent': ['condition', 'mutex', 'prodcons', 'threadring', 'chameneos
 inputs= {'mutex': '20000',
          'condition': '20000',
          'prodcons': '20000',
-         'threading': '50000000',
+         'threadring': '50000000',
          'chameneos': '6000000',
          'randmat': '0 8000',
          'thresh': '8000 1',
@@ -33,12 +33,12 @@ def run(results, sort, task, lang, num_workers):
                   stdout=PIPE, stderr=PIPE, 
                   shell=True)
     (out,err) = proc.communicate ()
-    print (out)
     os.chdir (os.path.join ('..', '..', '..'))
     t2 = time.time ()
 
     if lang == 'erlang':
-        lastline = string.split(out.strip(),'\n')[-1]
+        print(err)
+        lastline = string.split(err.strip(),'\n')[-1]
         tdiff = float(lastline)
     else:
         tdiff = t2 - t1
@@ -53,9 +53,9 @@ def main():
 
         for task in tasks[sort]:
             for lang in langs:
-                for thread in [4]:
+                for workers in [32]:
                     for i in range(1):
-                        run(results, sort, task, lang, thread)
+                        run(results, sort, task, lang, workers)
 
         with open(sort + '_results.csv', 'wb') as csv_file:
             perfwriter = csv.writer(csv_file, quoting=csv.QUOTE_MINIMAL)
