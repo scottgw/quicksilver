1c3761c43e4e86bf855be427aeddbc709d4e18c6
Scott West <scott.gregory.west@gmail.com>
2013-09-21 17:49:05 +0200

    Fixing haskell thresh.

--- benchmarks/parallel/haskell/thresh/Main.hs
+++ benchmarks/parallel/haskell/thresh/Main.hs
@@ -5,33 +5,29 @@ module Main where
 import           Control.DeepSeq
 import           Control.Parallel
 import           Control.Monad.Identity
+import           Control.Monad.ST
 
 import qualified Data.Array.Repa as Repa
 import           Data.Array.Repa (U, D, DIM2, (:.)(..), Z(..))
 import qualified Data.IntMap as Map
 import           Data.IntMap (IntMap)
 import qualified Data.Vector.Unboxed as Unbox
-import qualified Data.Vector.Unboxed.Mutable as Unbox (new)
+import qualified Data.Vector.Unboxed.Mutable as MUnbox
 import           Data.Word
 
 import           System.Environment
 
 thresh :: Int -> Int -> Unbox.Vector Int -> IO (Repa.Array U DIM2 Bool)
-thresh n thr v =
-    do !arrayMax <- Repa.foldAllP max 0 rv
+thresh n thr !v =
+    do !rv <- return (Repa.fromUnboxed (Z :. n :. n) v)
+       !arrayMax <- Repa.deepSeqArray rv (Repa.foldAllP max 0 rv)
        print arrayMax
        let
-         hist' = mkHist (Repa.toUnboxed rv)
-
-         hist :: IntMap Int
-         hist = Map.unionWith (+) hist
-                (Map.fromList [(i,0) | i <- [0 .. arrayMax]])
-
          limit = (n * n * thr) `div` 100
 
          lastNumber :: Int
-         lastNumber = either id (const 0)
-                      (foldr go (Right 0) [arrayMax, arrayMax - 1 .. 0])
+         !lastNumber = either id (const 0)
+                       (foldr go (Right 0) [arrayMax, arrayMax - 1 .. 0])
            where
              go !i (Right !s) | s >= limit = Left i
                               | otherwise  = Right (s + hist Map.! i)
@@ -39,17 +35,28 @@ thresh n thr v =
 
          threshArray :: Repa.Array D DIM2 Bool
          threshArray = Repa.map (< lastNumber) rv
-       Repa.computeP threshArray -- Repa.deepSeqArray rv threshArray)
+       
+       !res <- Repa.computeP threshArray
+       putStrLn "Foo"
+       return res
  where
-    mkHist :: Unbox.Vector Int -> IntMap Int
-    mkHist = Unbox.ifoldl' (\ m i v -> Map.insertWith (+) i v m) Map.empty
-    
-    rv :: Repa.Array U DIM2 Int
-    rv = Repa.fromUnboxed (Z :. n :. n) v
+    hist :: IntMap Int
+    hist = Map.fromList $ zip [0..99] (Unbox.toList $ Unbox.create mkHistMVec)
+      where
+        mkHistMVec :: ST s (Unbox.MVector s Int)
+        mkHistMVec =
+          do mvec <- MUnbox.replicate 100 0
+             let go 0 = return ()
+                 go !i =
+                   do let !vecVal = Unbox.unsafeIndex v i
+                      !count <- MUnbox.unsafeRead mvec vecVal
+                      MUnbox.unsafeWrite mvec vecVal (count + 1)
+                      go (i-1)
+             go (Unbox.length v)
+             return mvec
 
 main =
   do [n, thr] <- (map read) `fmap` getArgs
      let v = Unbox.replicate (n * n) 0
-     -- v <- Unbox.unsafeFreeze mv
-     x <- thresh n thr v
-     print (Unbox.length $ Repa.toUnboxed x)
+     !x <- thresh n thr v
+     print (x Repa.! (Z :. 42 :. 42))
