196ede621c846a07629a68a63851f6a79d05bec2
Scott West <scott.gregory.west@gmail.com>
2014-02-08 14:40:42 +0100

    Fixing usage of dyn_cast to use dyn_cast_or_null.
    We want to check that the instruction actually exists.

--- llvm_hoist_sync_pass/LiftSync.cpp
+++ llvm_hoist_sync_pass/LiftSync.cpp
@@ -328,7 +328,7 @@ namespace
 
     pq_node first_arg_call(Instruction *inst, StringRef str)
     {
-      if (CallInst *CI = dyn_cast<CallInst>(inst))
+      if (CallInst *CI = dyn_cast_or_null<CallInst>(inst))
         {
           Function *fun = CI->getCalledFunction();
           if (fun && fun->getName() == str)
